1b48026f56a1a1a7c4d62bfe81095a7cc162a994
Marcos Ortega <mortega@na-sa.com.ar>
2022-11-04 14:57:29 -0300

    animation

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grow,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { experimentalStyled as styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import "./styles/Home.css";
 import Grid from "@mui/material/Unstable_Grid2";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 import IconContrato from "../assets/images/iconos/firma-digital.png";
 
@@ -13,16 +22,90 @@ import ImageFM from "../assets/images/entherprise/FM23.jpg";
 import LogoFM from "../assets/images/entherprise/FM23-Light.png";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor:"#343338",
+  backgroundColor: "#343338",
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: "center",
   height: "100%",
-  
+
   color: theme.palette.text.secondary,
 }));
 
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0},
+};
+const BoxMotion = ({ num }) => {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  React.useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+    >
+      <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid xs={12} md={6}>
+              <Item sx={{ overflowY: "auto" }}>
+                <img
+                  style={{ maxHeight: "7vh" }}
+                  src={LogoFM}
+                  alt="bg"
+                  className="logoFm"
+                />
+                <Typography variant="caption" sx={{ color: "#e5e5e5" }}>
+                  UN JUEGO DE SIMULACIÓN INCOMPARABLE
+                  <br />
+                  <br />
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{ color: "#e5e5e5" }}
+                  gutterBottom
+                >
+                  Métete en el papel de un verdadero mánager dirigiendo a los
+                  mejores equipos de fútbol del planeta y juega al deporte rey a
+                  tu manera.
+                  <br />
+                  <br />
+                  Ya sea fichando mejor, entrenando más duro o siendo más
+                  inteligente que el rival, el arte de la gestión futbolística
+                  consiste en encontrar el margen de victoria.
+                  <br />
+                  <br />
+                  Football Manager es un juego en el que ningún sueño es
+                  demasiado grande y ni ninguna ambición demasiado extravagante.
+                  No te limites a cumplir las expectativas. Toma el control de
+                  tu club, sueña a lo grande y arrasa.
+                </Typography>
+              </Item>
+            </Grid>
+            <Grid  xs={12} md={6}>
+              <Item sx={{height:"300px",mb:7}}>
+                <img width={"100%"} height={"100%"} src={ImageFM} alt="bg" />
+              </Item>
+            </Grid>
+          </Grid>
+    </motion.div>
+  );
+};
 const Home = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
   return (
     <>
       <Box component="main" sx={{}}>
@@ -91,45 +174,12 @@ const Home = () => {
             </Box>
           </Box>
         </Box>
-        <Box component="section" sx={{ px: 3,height:400 }}>
-          <Grid
-            container
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            
-          >
-            <Grid xs={6} height={"100%"}>
-              <Item >
-                <img width={"50%"} height={"50%"} src={LogoFM} alt="bg" />
-                <Typography variant="h5" sx={{color:"#e5e5e5"}}>
-                  UN JUEGO DE SIMULACIÓN INCOMPARABLE
-                  <br />
-                  <br />
-                </Typography>
-                <Typography variant="subtitle1" sx={{color:"#e5e5e5"}}  gutterBottom>
-                  Métete en el papel de un verdadero mánager dirigiendo a los
-                  mejores equipos de fútbol del planeta y juega al deporte rey a
-                  tu manera.
-                  <br />
-                  <br />
-                  Ya sea fichando mejor, entrenando más duro o siendo más
-                  inteligente que el rival, el arte de la gestión futbolística
-                  consiste en encontrar el margen de victoria.
-                  <br />
-                  <br />
-                  Football Manager es un juego en el que ningún sueño es
-                  demasiado grande y ni ninguna ambición demasiado extravagante.
-                  No te limites a cumplir las expectativas. Toma el control de
-                  tu club, sueña a lo grande y arrasa.
-                </Typography>
-              </Item>
-            </Grid>
-            <Grid xs={6} height={"100%"}>
-              <Item >
-                <img width={"100%"} height={"100%"} src={ImageFM} alt="bg" />
-              </Item>
-            </Grid>
-          </Grid>
+        <Box component="section" sx={{ px: 3, my:5, height: 400 }}>
+        <BoxMotion num={1} />
         </Box>
+        
+{/*         <BoxMotion num={2} />
+        <BoxMotion num={3} /> */}
       </Box>
     </>
   );
